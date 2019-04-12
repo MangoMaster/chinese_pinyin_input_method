3754a13e9b2fa823d1295200ab3ef083852572c3
MangoMaster <weijd17@mails.tsinghua.edu.cn>
2019-04-12 13:27:18 +0800

    Accelerate and reduce memory usage 1. sqlite: Close synchronous and use transaction to speed up 2. pypinyin: Avoid copying to speed up and reduce memory usage 3. Analyze news one by one to reduce memory usage

--- src/bake_dataset.py
+++ src/bake_dataset.py
@@ -1,8 +1,11 @@
+import gc
 import json
+import os
 import re
+import sqlite3
+
 import jieba
 import pypinyin
-import sqlite3
 
 
 def get_news_text(news_file):
@@ -24,24 +27,23 @@ def get_news_text(news_file):
     return news_texts
 
 
-def get_words(texts):
+def get_words(text):
     """
     Cut text into words.
 
     Arg:
-        texts: A list of text, each text is a Chinese sentence.
+        text: A Chinese sentence.
 
     Returns:
-        A list of words cut from texts.
+        A list of words cut from text.
     """
     words = []
     # 只考虑中文字符
     chinese_words = re.compile(u"[\u4e00-\u9fa5]+")
-    for text in texts:
-        segments = jieba.cut(text)
-        for word in segments:
-            if re.match(chinese_words, word):
-                words.append(word)
+    segments = jieba.cut(text)
+    for word in segments:
+        if re.match(chinese_words, word):
+            words.append(word)
     return words
 
 
@@ -74,6 +76,10 @@ def count_pinyin_words(pinyin_word_pairs, database_path):
     try:
         connection = sqlite3.connect(database_path, isolation_level=None)
         cursor = connection.cursor()
+        # 关闭写同步以加速
+        cursor.execute("""pragma synchronous = off""")
+        # 显式开启事务以加速
+        cursor.execute("""begin transaction;""")
         for pinyin, word in pinyin_word_pairs:
             pinyin = ' '.join(pinyin)
             cursor.execute("""
@@ -86,24 +92,33 @@ def count_pinyin_words(pinyin_word_pairs, database_path):
                     insert into PinyinWord (pinyin, word, count)
                     values ('%s', '%s', 1);
                     """ % (pinyin, word))
+        cursor.execute("""commit transaction;""")
         connection.commit()
     finally:
         cursor.close()
         connection.close()
 
 
-"""
-Convert sina news to pinyin-word counts
-"""
-if __name__ == "__main__":
-    import os
+def bake_dataset_pinyin_word():
+    # 无自定义拼音库，控制pypinyin不做copy操作, 以减少内存占用
+    os.environ['PYPINYIN_NO_DICT_COPY'] = 'true'
+    # 设置文件和数据库路径
     news_dirname = "../data/sina_news_utf8"
     news_filenames = os.listdir(news_dirname)
     database_path = "../data/counter.db"
     for news_filename in news_filenames:
-        print("Reading " + news_filename)
         with open(os.path.join(news_dirname, news_filename), 'r') as f:
+            print("Reading " + news_filename + "...")
             news_texts = get_news_text(f)
-            news_words = get_words(news_texts)
-            news_pinyin_word_pairs = get_pinyin(news_words)
-            count_pinyin_words(news_pinyin_word_pairs, database_path)
+            # 逐条分析新闻以减少内存占用
+            for news_text in news_texts:
+                news_words = get_words(news_text)
+                news_pinyin_word_pairs = get_pinyin(news_words)
+                count_pinyin_words(news_pinyin_word_pairs, database_path)
+
+
+"""
+Convert sina news to pinyin-word counts
+"""
+if __name__ == "__main__":
+    bake_dataset_pinyin_word()
