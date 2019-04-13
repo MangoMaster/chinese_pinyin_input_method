6391e1a7d08219acef59e9359ec3c30cb6d8d3c7
MangoMaster <weijd17@mails.tsinghua.edu.cn>
2019-04-13 17:07:26 +0800

    Change test from .sh to .py

--- test/calculate_accuracy.py
+++ test/calculate_accuracy.py
@@ -1,14 +1,18 @@
 import os
 
-return_value = os.system(os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "test_pinyin.sh"))
+executable_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "src", "word2", "pinyin.py")
+input_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "data", "input1.txt")
+output_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "data", "output1.txt")
+std_output_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "data", "std_output1.txt")
+return_value = os.system("python3 " + executable_file_path + " " +
+                         input_file_path + " " + output_file_path)
 if return_value == 0:
     total_number = 0
     accurate_number = 0
-    output_file_path = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "data", "output.txt")
-    std_output_file_path = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "data", "std_output.txt")
     with open(output_file_path, 'r') as output_file, open(std_output_file_path, 'r') as std_output_file:
         for output, std_output in zip(output_file, std_output_file):
             if output == std_output:
@@ -19,4 +23,4 @@ if return_value == 0:
     print("Accuracy:", accurate_number, "/", total_number,
           "=", accurate_number / total_number * 100, "%")
 else:
-    print("test_pinyin.sh is not executed properly.")
+    print("Pinyin program is not executed properly.")
--- test/test_pinyin.sh
+++
@@ -1,2 +0,0 @@
-BASEDIRPATH=$(dirname $0)
-python3 $BASEDIRPATH/../src/word2/pinyin.py $BASEDIRPATH/../data/input.txt $BASEDIRPATH/../data/output.txt
