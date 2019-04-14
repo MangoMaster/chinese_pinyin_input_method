60950c29283485832f72f3eed5c705b19d24f70b
MangoMaster <weijd17@mails.tsinghua.edu.cn>
2019-04-14 10:49:45 +0800

    Fix bug in build_table: Miss key's probability is wrong Abondon check_table and move it into build_table

--- src/char2/build_table.py
+++ src/char2/build_table.py
@@ -16,8 +16,9 @@ def build_table(pinyin_char_database_path, pinyin_pinyin_char_char_database_path
 
     Returns:
         pinyin-char table: A dict, key->pinyin, value->[[char, log(probability)]].
-        char-char table: A dict, key->str(char1-char2), value->log(probability)
+        char-char table: A dict, key->str(char1-char2), value->log(probability).
     """
+    # Build pinyin-char table
     pinyin_char_table = dict()
     pinyin_char_count = 0
     try:
@@ -38,15 +39,25 @@ def build_table(pinyin_char_database_path, pinyin_pinyin_char_char_database_path
                     pinyin_char_table[pinyin].append([char, count])
                 else:
                     pinyin_char_table[pinyin] = [[char, count], ]
-        # Change count to log(probability)
-        for pinyin, char_list in pinyin_char_table.items():
-            char_list = [(char, math.log10(count / pinyin_char_count))
-                         for char, count in char_list]
-            pinyin_char_table[pinyin] = char_list
     finally:
         cursor.close()
         connection.close()
+    # Make sure all the possible pinyin of a single Chinese character
+    # is in pinyin_char_table.
+    pinyin_character_table = {'nu': '努', 'bo': '博', 'kang': '康', 'nian': '年', 'bin': '宾', 'liang': '两', 'ni': '你', 'ka': '卡', 'huang': '黄', 'zen': '怎', 'gou': '构', 'dan': '但', 'shu': '数', 'chuo': '戳', 'sun': '损', 'zhang': '长', 'sa': '萨', 'dang': '当', 'hang': '行', 'fo': '佛', 'hong': '红', 'hei': '黑', 'man': '满', 'ming': '名', 'lu': '路', 'lia': '俩', 'lei': '类', 'ye': '业', 'ling': '领', 'qie': '切', 'suo': '所', 'ha': '哈', 'chou': '筹', 'gang': '港', 'mei': '美', 'jue': '决', 'xi': '系', 'nei': '内', 'guang': '广', 'cai': '才', 'chang': '场', 'qiao': '桥', 'cheng': '成', 'du': '度', 'lian': '联', 'ping': '平', 'gen': '根', 'sheng': '生', 'luan': '乱', 'zhong': '中', 'xiao': '小', 'dao': '到', 'sen': '森', 'zhuan': '专', 'kai': '开', 'tao': '套', 'wa': '挖', 'da': '大', 'bian': '变', 'zhu': '主', 'ri': '日', 'nin': '您', 'ya': '亚', 'rao': '绕', 'kao': '考', 'mi': '米', 'can': '参', 'pu': '普', 'bai': '百', 'shun': '顺', 'shui': '水', 'lo': '咯', 'mu': '目', 'jing': '经', 'e': '额', 'nan': '南', 'mo': '模', 'pa': '怕', 'jiu': '就', 'xue': '学', 'liao': '了', 'lv': '旅', 'nue': '虐', 'zhou': '州', 'zou': '走', 'zhe': '这', 'gei': '给', 'chai': '拆', 'shua': '刷', 'zong': '总', 'nie': '聂', 'cang': '藏', 'beng': '崩', 'ma': '马', 'yan': '研', 'na': '那', 'le': '了', 'pou': '剖', 'min': '民', 'bei': '被', 'chuang': '创', 'sha': '沙', 'cong': '从', 'cen': '岑', 'wu': '务', 'cui': '崔', 'chu': '出', 'wang': '网', 'zhuang': '装', 'peng': '朋', 'nao': '脑', 'qin': '亲', 'pan': '判', 'ci': '次', 'wai': '外', 'se': '色', 'tai': '台', 'neng': '能', 'cou': '凑', 'nv': '女', 'jie': '解', 'dun': '顿', 'gu': '故', 'ning': '宁', 'jian': '建', 'han': '汉', 'en': '恩', 'run': '润', 'mie': '灭', 'zang': '脏', 'shan': '山', 'shuo': '说', 'zi': '自', 'chun': '春', 'shuan': '栓', 'xing': '行', 'zha': '诈', 'gong': '工', 'ji': '机', 'tuan': '团', 'hu': '户', 'fa': '发', 'chuai': '揣', 'bie': '别', 'dian': '点', 'fang': '方', 'cuan': '窜', 'biao': '标', 'zhun': '准', 'san': '三', 'wan': '万', 'ruan': '软', 'zu': '组', 'qia': '恰', 'zao': '造', 'kan': '看', 'cha': '查', 'tou': '投', 'xian': '现', 'qi': '其', 'sang': '桑', 'dia': '嗲', 'rong': '融', 'shang': '上', 'xiu': '修', 'gun': '滚', 'nuan': '暖', 'yi': '一', 'pin': '品', 'cu': '促', 'miao': '苗', 'ku': '库', 'nang': '囊', 'pie': '撇', 'zheng': '政', 'pai': '排', 'keng': '坑', 'bi': '比', 're': '热', 'weng': '翁', 'qiang': '强', 'pi': '批', 'yun': '运', 'men': '们', 'juan': '捐', 'ren': '人', 'dui': '对', 'bu': '不', 'shou': '手', 'tong': '通', 'teng': '腾', 'ga': '尬', 'ba': '把', 'zhuo': '着', 'lai': '来', 'rang': '让', 'suan': '算', 'jiong': '窘', 'jun': '军', 'mai': '买', 'dei': '得', 'shi': '是',
+                              'zun': '尊', 'seng': '僧', 'mou': '某', 'bang': '帮', 'rou': '肉', 'ru': '入', 'yo': '哟', 'kou': '口', 'que': '确', 'te': '特', 'cao': '草', 'ta': '他', 'xin': '新', 'hui': '会', 'chen': '陈', 'chuan': '传', 'gan': '干', 'ang': '昂', 'shei': '谁', 'sao': '扫', 'gai': '改', 'nen': '嫩', 'de': '的', 'ken': '肯', 'ai': '爱', 'zuo': '作', 'zhui': '追', 'pen': '喷', 'xiang': '项', 'qing': '情', 'a': '阿', 'yao': '要', 'kui': '馈', 'pang': '旁', 'piao': '票', 'ran': '然', 'xia': '下', 'cun': '村', 'huai': '怀', 'lang': '朗', 'song': '送', 'lin': '林', 'er': '而', 'hua': '化', 'fu': '服', 'shen': '深', 'shuai': '率', 'geng': '更', 'ceng': '层', 'fou': '否', 'ting': '庭', 'feng': '风', 'kuan': '款', 'tui': '推', 'zhao': '照', 'guan': '关', 'jiao': '交', 'xuan': '选', 'o': '哦', 'jiang': '将', 'meng': '盟', 'zai': '在', 'lan': '兰', 'ca': '擦', 'tiao': '条', 'fan': '范', 'you': '有', 'gui': '规', 'hun': '婚', 'diao': '调', 'xie': '些', 'shuang': '双', 'xun': '讯', 'wo': '我', 'po': '破', 'ban': '办', 'zhuai': '拽', 'zei': '贼', 'chong': '重', 'gua': '挂', 'mian': '面', 'luo': '落', 'ne': '呢', 'kuo': '括', 'quan': '全', 'an': '安', 'zhua': '抓', 'hao': '好', 'qiong': '穷', 'dou': '都', 'gao': '高', 'tu': '图', 'fei': '费', 'zhai': '债', 'su': '速', 'si': '司', 'lie': '列', 'zhan': '展', 'zhen': '镇', 'lou': '楼', 'shai': '晒', 'duo': '多', 'fen': '分', 'sou': '搜', 'heng': '衡', 'lun': '论', 'lue': '略', 'dong': '动', 'reng': '仍', 'chi': '持', 'kuang': '况', 'ju': '据', 'jia': '家', 'huo': '活', 'qiu': '求', 'ding': '定', 'pao': '跑', 'miu': '谬', 'kua': '跨', 'guai': '怪', 'ben': '本', 'che': '车', 'jin': '进', 'bing': '并', 'nuo': '诺', 'dai': '代', 'pian': '片', 'kong': '空', 'di': '地', 'zhi': '制', 'niao': '鸟', 'tang': '堂', 'hai': '还', 'nai': '奶', 'hen': '很', 'tuo': '脱', 'yuan': '员', 'tian': '天', 'zeng': '增', 'tie': '铁', 'ruo': '若', 'zui': '最', 'zhei': '这', 'sui': '随', 'tun': '吞', 'kuai': '快', 'lao': '老', 'diu': '丢', 'ze': '责', 'la': '拉', 'leng': '冷', 'wen': '文', 'zuan': '钻', 'nong': '农', 'yue': '月', 'xu': '需', 'niang': '娘', 'ou': '欧', 'ti': '题', 'long': '龙', 'tei': '忒', 'nou': '耨', 'yang': '样', 'za': '杂', 'li': '理', 'guo': '国', 'duan': '段', 'niu': '牛', 'me': '么', 'liu': '流', 'yong': '用', 'yin': '因', 'wei': '为', 'ying': '应', 'she': '设', 'qu': '区', 'sai': '赛', 'chao': '超', 'kun': '困', 'bao': '报', 'shao': '少', 'ce': '策', 'hou': '后', 'pei': '配', 'mao': '贸', 'huan': '环', 'xiong': '雄', 'qun': '群', 'ke': '可', 'rui': '瑞', 'he': '和', 'mang': '忙', 'zan': '赞', 'yu': '于', 'ge': '个', 'cuo': '措', 'deng': '等', 'die': '跌', 'ao': '奥', 'qian': '前', 'chui': '吹', 'ei': '诶', 'tan': '谈', 'chan': '产'}
+    for key, value in pinyin_character_table.items():
+        if not key in pinyin_char_table:
+            # Reduce an order of magnitude -> 0.1
+            pinyin_char_table[key] = [[value, 0.1], ]
+            print("Miss: " + key + " Set to: " + value)
+    # Change count to log(probability)
+    for pinyin, char_list in pinyin_char_table.items():
+        char_list = [(char, math.log10(count / pinyin_char_count))
+                     for char, count in char_list]
+        pinyin_char_table[pinyin] = char_list
 
+    # Build char-char table
     char_char_table = dict()
     char_char_count = 0
     try:
@@ -68,39 +79,14 @@ def build_table(pinyin_char_database_path, pinyin_pinyin_char_char_database_path
                     char_char_table[char_pair] += count
                 else:
                     char_char_table[char_pair] = count
-        # Change count to log(probability)
-        for char_char, count in char_char_table.items():
-            char_char_table[char_char] = math.log10(count / char_char_count)
     finally:
         cursor.close()
         connection.close()
+    # Change count to log(probability)
+    for char_char, count in char_char_table.items():
+        char_char_table[char_char] = math.log10(count / char_char_count)
 
     return pinyin_char_table, char_char_table
-
-
-def check_table(pinyin_char_table):
-    """
-    Check pinyin_char_table to be properly set.
-
-    Args:
-        pinyin_char_table: A dict, key->pinyin, value->[(char, log(probability))].
-    """
-    # Make sure all the possible pinyin of a single Chinese character
-    # is in pinyin_char_table.
-    pinyin_character_table = {'cui': '崔', 'jiu': '就', 'dun': '吨', 'gui': '贵', 'nian': '年', 'que': '却', 'yue': '月', 'nuo': '诺', 'guai': '拐', 'kui': '亏', 'zen': '怎', 'lue': '略', 'suo': '所', 'ben': '本', 'bin': '斌', 'teng': '疼', 'ming': '名', 'nie': '捏', 'nv': '女', 'cuo': '错', 'wa': '挖', 'duan': '段', 'mo': '末', 'chao': '超', 'pao': '跑', 'qiao': '桥', 'bie': '别', 'xue': '学', 'bo': '波', 'ba': '把', 'gang': '刚', 'fou': '否', 'pa': '怕', 'ka': '卡', 'lu': '路', 'du': '度', 'song': '送', 'lun': '轮', 'shen': '深', 'sa': '撒', 'mang': '忙', 'gai': '该', 'xia': '下', 'shan': '山', 'geng': '更', 'ding': '定', 'heng': '横', 'kun': '崑', 'jia': '家', 'wen': '问', 'ou': '欧', 'jiao': '较', 'mu': '亩', 'zhu': '住', 'gu': '古', 'ei': '诶', 'la': '拉', 'lang': '琅', 'gan': '干', 'sen': '森', 'you': '有', 'su': '苏', 'diu': '丢', 'neng': '能', 'lei': '类', 'can': '餐', 'ti': '提', 'fei': '非', 'zhuai': '拽', 'ken': '啃', 'duo': '多', 'cuan': '窜', 'liao': '聊', 'ting': '听', 'xian': '县', 'chi': '吃', 'dui': '对', 'tuan': '团', 'nai': '奶', 'luan': '乱', 'dei': '得', 'ang': '昂', 'chan': '产', 'deng': '等', 'jun': '均', 'yong': '用', 'xi': '系', 'hong': '红', 'zhi': '至', 'ma': '吗', 'lo': '咯', 'jue': '绝', 'pian': '骗', 'dou': '都', 'ci': '次', 'pou': '抔', 'qie': '且', 'ge': '个', 'zhen': '镇', 'cang': '藏', 'liang': '两', 'qing': '请', 'ze': '则', 'zhai': '寨', 'he': '和', 'sha': '啥', 'bai': '白', 'hei': '黑', 'shu': '属', 'te': '特', 'ye': '也', 'lan': '蓝', 'da': '大', 'chuang': '创', 'tiao': '条', 'cun': '村', 'fu': '副', 'qu': '去', 'dan': '但', 'fo': '佛', 'guang': '光', 'nan': '难', 'kai': '开', 'wai': '外', 'kan': '看', 'nu': '怒', 'sui': '岁', 'miao': '秒', 'lai': '来', 'zhang': '张', 'me': '么', 'ban': '办', 'zu': '组', 'long': '龙', 'ji': '及', 'pu': '铺', 'feng': '风', 'shuai': '衰', 'po': '破', 'dang': '党', 'zheng': '正', 'hu': '户', 'ru': '如', 'qian': '前', 'kuang': '矿', 'nen': '嫩', 'shua': '刷', 'meng': '梦', 'zeng': '增', 'yao': '要', 'ya': '牙', 'zhuan': '转', 'dong': '东', 'ruo': '若', 'men': '们', 'quan': '全', 'man': '满', 'nuan': '暖', 'ai': '爱', 'pi': '批', 'shuang': '双', 'gua': '挂', 'mai': '买', 'dao': '到', 'gei': '给', 'huai': '坏', 'kou': '口', 'huo': '或', 'she': '摄', 'chun': '纯', 'miu': '谬', 'tuo': '拖', 'jie': '届', 'chui': '吹', 'guan': '馆', 'kong': '控', 'shui': '水', 'dai': '带', 'sai': '赛', 'hua': '化', 'rao': '绕', 'chuan': '穿', 'cai': '才', 'lv': '率', 'piao': '票', 'bing': '并', 'niang': '娘', 'jin': '近', 'weng': '翁', 'zui': '最', 'beng': '泵', 'xing': '性', 'ku': '哭', 'sou': '艘', 'zhan': '站', 'jing': '经', 'qiang': '强', 'ren': '人', 'che': '车', 'peng': '彭', 'wo': '我', 'biao': '表',
-                              'nin': '您', 'han': '含', 'shao': '少', 'li': '里', 'pang': '旁', 'lou': '楼', 'shun': '顺', 'jian': '件', 'gun': '丨', 'ning': '宁', 'yang': '杨', 'zou': '走', 'hao': '好', 'chai': '拆', 'zai': '在', 'chen': '陈', 'zhei': '这', 'wang': '网', 'cha': '茶', 'yi': '以', 'tai': '太', 'zha': '扎', 'zhuang': '撞', 'gou': '狗', 'cheng': '称', 'guo': '过', 'niu': '牛', 'shei': '谁', 'chuo': '戳', 'di': '第', 'shuan': '拴', 'zuo': '做', 'le': '了', 'fan': '反', 'xiong': '熊', 'pai': '拍', 'a': '啊', 'qiong': '穷', 'diao': '掉', 'zhe': '这', 'chou': '抽', 'cen': '涔', 'tun': '屯', 'se': '色', 'en': '恩', 'tou': '头', 'zhua': '抓', 'sao': '扫', 'ying': '应', 'huang': '黄', 'chuai': '踹', 'qun': '群', 'yun': '云', 'rong': '蓉', 'ce': '侧', 're': '热', 'pei': '陪', 'nou': '耨', 'wu': '无', 'juan': '卷', 'yo': '哟', 'zi': '自', 'gao': '高', 'yan': '严', 'nong': '弄', 'mei': '没', 'ao': '奥', 'hen': '很', 'ri': '日', 'mie': '灭', 'ga': '伽', 'zei': '贼', 'de': '的', 'si': '四', 'zong': '总', 'bi': '比', 'niao': '鸟', 'ruan': '软', 'ju': '据', 'cong': '从', 'zhong': '中', 'zhao': '找', 'kuan': '款', 'mao': '猫', 'na': '那', 'dia': '嗲', 'san': '三', 'zao': '早', 'er': '而', 'rui': '睿', 'tong': '同', 'zun': '尊', 'kuai': '快', 'chang': '场', 'fen': '分', 'ping': '坪', 'cou': '凑', 'ta': '他', 'hai': '还', 'bian': '便', 'hou': '后', 'qia': '掐', 'hun': '混', 'die': '跌', 'gen': '跟', 'sheng': '省', 'zuan': '钻', 'dian': '点', 'yu': '与', 'bang': '帮', 'shi': '是', 'tang': '躺', 'ha': '哈', 'rang': '让', 'nei': '内', 'chu': '出', 'kua': '跨', 'pen': '喷', 'pie': '撇', 'wei': '为', 'shang': '上', 'tan': '谈', 'ke': '可', 'pin': '拼', 'nang': '囊', 'zhou': '周', 'ne': '呢', 'zhuo': '桌', 'hang': '杭', 'qi': '其', 'sang': '桑', 'reng': '仍', 'shou': '受', 'tu': '图', 'keng': '坑', 'suan': '算', 'jiong': '囧', 'nue': '虐', 'run': '润', 'jiang': '将', 'cu': '促', 'zang': '脏', 'ling': '另', 'kuo': '扩', 'liu': '刘', 'hui': '会', 'bao': '报', 'xun': '讯', 'kao': '靠', 'chong': '冲', 'min': '民', 'ceng': '曾', 'mian': '面', 'zan': '赞', 'seng': '僧', 'ca': '擦', 'sun': '孙', 'huan': '换', 'wan': '万', 'zhui': '追', 'xiang': '向', 'lia': '俩', 'tian': '天', 'o': '哦', 'shuo': '说', 'ni': '你', 'fa': '法', 'tui': '推', 'zhun': '准', 'qiu': '球', 'qin': '亲', 'yuan': '原', 'an': '按', 'yin': '因', 'xin': '新', 'mou': '某', 'xie': '写', 'tao': '套', 'fang': '房', 'tie': '贴', 'lian': '连', 'ran': '然', 'xiu': '秀', 'lin': '林', 'nao': '闹', 'xiao': '小', 'luo': '罗', 'mi': '米', 'rou': '肉', 'cao': '曹', 'shai': '晒', 'kang': '抗', 'xuan': '选', 'bei': '被', 'leng': '冷', 'lie': '列', 'lao': '老', 'xu': '需', 'gong': '共', 'pan': '潘', 'tei': '忒', 'e': '俄', 'bu': '不', 'za': '砸'}
-    for key, value in pinyin_character_table.items():
-        if not key in pinyin_char_table:
-            pinyin_char_table[key] = [[value, 1], ]
-            print("Miss: " + key + " Set to: " + value)
-        else:
-            max_char = ['', float("-inf")]
-            for char, probability in pinyin_char_table[key]:
-                if probability > max_char[1]:
-                    max_char = (char, probability)
-            assert max_char[0] != ''
-            pinyin_character_table[key] = max_char[0]
-    print(pinyin_character_table)
 
 
 def save_table(table, table_path):
@@ -129,6 +115,5 @@ if __name__ == "__main__":
         os.path.dirname(os.path.realpath(__file__)), os.path.pardir, os.path.pardir, "data", "char_char_table.json")
     pinyin_char_table, char_char_table = build_table(
         pinyin_char_database_path, pinyin_pinyin_char_char_database_path)
-    check_table(pinyin_char_table)
     save_table(pinyin_char_table, pinyin_char_table_path)
     save_table(char_char_table, char_char_table_path)
